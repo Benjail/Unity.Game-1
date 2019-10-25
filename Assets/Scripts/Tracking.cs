678cf4181a0f30b6883bb3497adb31a8f8c0c0a8
Benjail <miha203@mail.ru>
2019-10-25 12:33:25 +0300

    Убрал TMP, переименовал файлы

---
+++ Assets/Scripts/Tracking.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tracking : MonoBehaviour
+{
+    public float Speed;
+    public GameObject GameObject;
+    public GameObject border;//граница   
+    public float marginY;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = Vector3.Lerp(transform.position, new Vector3(GameObject.transform.position.x, GameObject.transform.position.y+marginY, transform.position.z),Speed*Time.deltaTime);
+    }
+}
